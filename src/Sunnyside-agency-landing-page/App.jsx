c68eb56ec6125b94497b534f785cda3582a8871b
Indrani Das <Subramanyam.Dindukurthi@minutestoseconds.com>
2022-09-14 09:50:20 +0530

    day-3-challanges

---
+++ src/Sunnyside-agency-landing-page/App.jsx
@@ -0,0 +1,9 @@
+export const App = () => {
+    return (
+        <>
+            <h1>
+                Sunnyside agency landing page
+            </h1>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { App } from './Advice-generator-app/App';
-import {App} from './REST-Countries-API/App';
+// import {App} from './REST-Countries-API/App';
+import {App} from './Sunnyside-agency-landing-page/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
